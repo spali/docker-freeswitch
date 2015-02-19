66937cf6bc4711dae1ccc7fd464998e7f39dc312
Thomas Spalinger <spali@spali.ch>
2015-02-19 16:56:13 +0100

    Update freeswitch_setup.sh

--- freeswitch_setup.sh
+++ freeswitch_setup.sh
@@ -5,8 +5,8 @@ set -e
 
 # copy vanilla config if no config is found
 if [ ! -f /etc/freeswitch/freeswitch.xml ]; then
-  cp -a /usr/share/freeswitch/conf/vanilla /etc/freeswitch
+  cp -a /usr/share/freeswitch/conf/vanilla/* /etc/freeswitch/
   # fix permissions and ownership
   chown -R freeswitch:freeswitch /etc/freeswitch
-  chmod 700 /etc/freeswitch
+  chmod -R 700 /etc/freeswitch
 fi
